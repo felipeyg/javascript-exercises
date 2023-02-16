6a6b1e9fcf4840d8f01063fa68554efe7c07ffdd
felipeyg <felipeyg@DESKTOP-4SI6R2E.localdomain>
2023-02-16 00:11:26 -0300

    add exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
-};
+const reverseString = function(string) {
+    const reversedString = new Array();
+    for (let i = string.length; i >= 0; i--) {
+        stringArray = string.split("");
+        reversedString.push(stringArray[i]);
+    } return reversedString.join("");
+}
 
 // Do not edit below this line
 module.exports = reverseString;
