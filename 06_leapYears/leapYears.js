15f7337287f25648930455205d2c08fa7fbcfabd
felipeyg <felipeyg@DESKTOP-4SI6R2E.localdomain>
2023-02-23 22:42:41 -0300

    add exercises 05 and 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,21 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    } else return false;
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+
+// const leapYears = function(year) {
+//   return year % 4 === 0 && ( year % 100 !== 0 || year % 400 === 0);
+// };
